a4ab592057d4840968ba1848584499babcb81cbc
Liu Ray <rray12tw346@gmail.com>
2023-05-27 19:48:53 +0800

    update puppeteer

--- crawler.js
+++ crawler.js
@@ -1,11 +1,16 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox']
-  });
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://www.google.com');
-  await browser.close();
+
+  await page.goto('https://developer.chrome.com/');
+
+  // Set screen size
+  await page.setViewport({width: 1080, height: 1024});
+
+  setTimeout(async () => {
+    await page.screenshot({path: 'screenshot.png'});
+    await browser.close();
+  }, 5000);
 })();
